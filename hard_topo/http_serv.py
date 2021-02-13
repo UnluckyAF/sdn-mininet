8a9b40803459008534c8a1ce9c357ea04d72b68e
UnluckyAF <kirill.barukhov@yandex.ru>
2021-02-13 16:21:07 +0300

    add correct path + fix bugs

--- hard_topo/http_serv.py
+++ hard_topo/http_serv.py
@@ -3,12 +3,23 @@ from http.client import HTTPConnection
 import random
 import argparse
 
+
 key = random.randrange(10) + 1
 mod = random.randrange(10) + 1
 host_num = -1
 
 class handler(BaseHTTPRequestHandler):
+    global key, mod, host_num
+    print(key, mod, host_num)
+    key = key
+    mod = mod
+    host_num = host_num
+    print(key, mod, host_num)
+
+
     def do_GET(self):
+        global key, mod, host_num
+        print("GET")
         self.send_response(200)
         self.send_header('Content-type', 'text-html')
         self.send_header("host", str(host_num))
@@ -16,30 +27,59 @@ class handler(BaseHTTPRequestHandler):
         self.wfile.write("key={}".format(key).encode('utf-8'))
 
     def do_POST(self):
+        path = {
+            "0/1": 2,
+            "1/2": 0,
+            "2/0": 3,
+            "0/3": 4,
+            "3/4": 5,
+            "4/5": 6,
+            "5/6": 3,
+            "6/3": 7,
+            "3/7": 8,
+            "7/8": 3,
+            "8/3": 9
+        }
+        global key, mod, host_num
+        print("POST")
         self.send_response(200)
         self.end_headers()
-        post_key = int(self.path.split("/"))
+        print(self.path.split("/"))
+        post_key = int(self.path.split("/")[1])
+        #print(key, post_key, mod)
         key = key * post_key % mod
+        print(key, post_key, mod)
         self.wfile.write("{}".format(key).encode('utf-8'))
 
-        host_num = (int(self.headers["src"]) + 1) % 10
-        dst = (int(self.headers["src"]) + 2) % 10 
+        host_num = int(self.headers["dst"])
+
+        dst = 0
+        for k in path:
+            sr, me = k.split("/")
+            if sr == self.headers["src"] and me == str(host_num):
+                dst = path[k]
+                break
+        #dst = (int(self.headers["src"]) + 2) % 10
+        print(host_num, dst, key)
         conn = HTTPConnection("10.0.0.{}".format(dst + 1), 8000)
-        conn.request("POST", "/{}".format(key), headers={"src": host_num})
+        conn.request("POST", "/{}".format(key + 1), headers={"src": host_num, "dst": dst})
         response = conn.getresponse()
         print(response.status, response.reason, response.read)
         conn.close()
 
 
 def run(server_class=HTTPServer, handler_class=handler, init=False):
+    global key, mod, host_num
     if init:
+        print("INITIATOR")
         host_num = 0
         conn = HTTPConnection("10.0.0.2", 8000)
-        conn.request("POST", "/{}".format(key), headers={"src": host_num})
+        conn.request("POST", "/{}".format(key + 1), headers={"src": host_num, "dst": 1})
         response = conn.getresponse()
         conn.close()
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
+    print("RUNNING")
     httpd.serve_forever()
 
 if __name__ == '__main__':
@@ -48,5 +88,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     init = args.init
 
-    run()
-
+    run(init=init)
