709a894ce3e53ad471ae94900de42da75e4e6c71
UnluckyAF <kirill.barukhov@yandex.ru>
2021-04-26 02:33:03 +0300

    add flow (low rtt break)

--- hard_topo/http_serv.py
+++ hard_topo/http_serv.py
@@ -1,30 +1,35 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from http.client import HTTPConnection
-import random
+from multiprocessing import Process
 import argparse
+import logging
+import random
+import time
 
 
-key = random.randrange(10) + 1
-mod = random.randrange(10) + 1
+#key = random.randrange(10) + 1
+#mod = random.randrange(10) + 1
 host_num = -1
 
 class handler(BaseHTTPRequestHandler):
-    global key, mod, host_num
-    print(key, mod, host_num)
-    key = key
-    mod = mod
+    global host_num
+    #print(key, mod, host_num)
+    print(host_num)
+    #key = key
+    #mod = mod
     host_num = host_num
-    print(key, mod, host_num)
+    #print(key, mod, host_num)
 
 
     def do_GET(self):
-        global key, mod, host_num
+        #global key, mod, host_num
+        global host_num
         print("GET")
         self.send_response(200)
         self.send_header('Content-type', 'text-html')
         self.send_header("host", str(host_num))
         self.end_headers()
-        self.wfile.write("key={}".format(key).encode('utf-8'))
+        #self.wfile.write("key={}".format(key).encode('utf-8'))
 
     def do_POST(self):
         path = {
@@ -40,18 +45,25 @@ class handler(BaseHTTPRequestHandler):
             "7/8": 3,
             "8/3": 9
         }
-        global key, mod, host_num
+        #global key, mod, host_num
+        global host_num
         print("POST")
         self.send_response(200)
         self.end_headers()
-        print(self.path.split("/"))
-        post_key = int(self.path.split("/")[1])
+        content_len = int(self.headers.get('Content-Length'))
+        post_body = self.rfile.read(content_len)
+        logging.info("in POST: %s", post_body)
+        #print(self.path.split("/"))
+        #post_key = int(self.path.split("/")[1])
         #print(key, post_key, mod)
-        key = key * post_key % mod
-        print(key, post_key, mod)
-        self.wfile.write("{}".format(key).encode('utf-8'))
+        #key = key * post_key % mod
+        #print(key, post_key, mod)
+        self.wfile.write("ok {}".format(post_body).encode('utf-8'))
 
-        host_num = int(self.headers["dst"])
+        if host_num == -1:
+            host_num = int(self.headers["dst"])
+        elif host_num != int(self.headers["dst"]):
+            logging.error("wrong host dst from %d", self.headers["src"])
 
         dst = 0
         for k in path:
@@ -60,32 +72,56 @@ class handler(BaseHTTPRequestHandler):
                 dst = path[k]
                 break
         #dst = (int(self.headers["src"]) + 2) % 10
-        print(host_num, dst, key)
+        #print(host_num, dst, key)
+        logging.debug("host_num=%d, dst=%d", host_num, dst) 
+        logging.debug("about to start connection 10.0.0.%d", dst + 1)
         conn = HTTPConnection("10.0.0.{}".format(dst + 1), 8000)
-        conn.request("POST", "/{}".format(key + 1), headers={"src": host_num, "dst": dst})
+        conn.set_debuglevel(1)
+        logging.debug("posting to 10.0.0.%d, dst: %d", dst + 1, dst)
+        conn.request("POST", "", body=post_body, headers={"src": host_num, "dst": dst})
         response = conn.getresponse()
-        print(response.status, response.reason, response.read)
+        logging.debug("STATUS: %s, REASON: %s", response.status, response.reason)
+        #logging.debug("RESPONSE: %s", response.read())
+        #print(response.status, response.reason, response.read)
+        logging.debug("about to CLOSE connection 10.0.0.%d", dst + 1)
         conn.close()
+
+def spam():
+    global host_num
+    logging.debug("about to start connection 10.0.0.2")
+    conn = HTTPConnection("10.0.0.2", 8000)
+    conn.set_debuglevel(1)
+    while True:
+        logging.debug("posting to 10.0.0.2, dst: 1")
+        conn.request("POST", "", body="test", headers={"src": host_num, "dst": 1})
+        response = conn.getresponse()
+        logging.debug("STATUS: %s, REASON: %s", response.status, response.reason)
+        #logging.debug("RESPONSE: %s", response.read())
+        #print(response.status, response.reason, response.read)
+        time.sleep(0.2)
+    logging.debug("about to CLOSE connection 10.0.0.2")
+    conn.close()
 
 
 def run(server_class=HTTPServer, handler_class=handler, init=False):
-    global key, mod, host_num
+    global host_num
     if init:
-        print("INITIATOR")
+        logging.info("INITIATOR")
         host_num = 0
-        conn = HTTPConnection("10.0.0.2", 8000)
-        conn.request("POST", "/{}".format(key + 1), headers={"src": host_num, "dst": 1})
-        response = conn.getresponse()
-        conn.close()
+        p = Process(target=spam, daemon=True)
+        p.start()
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
-    print("RUNNING")
+    logging.info("RUNNING")
     httpd.serve_forever()
+    #p.join()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='http server doing something.')
     parser.add_argument("--init", action="store_true", help="if true the server is an initiator.")
+    parser.add_argument("--log-level", default=logging.INFO, type=lambda x: getattr(logging, x), help="Configure the logging level.")
     args = parser.parse_args()
+    logging.basicConfig(level=args.log_level)
     init = args.init
 
     run(init=init)
