8a3acdda81150d94e44840ac9518863badd96a38
UnluckyAF <kirill.barukhov@yandex.ru>
2021-05-06 04:57:32 +0300

    fix graph picture

--- graph/main.go
+++ graph/main.go
@@ -11,6 +11,7 @@ import (
 
 var path = map[string]int {
     "0/1": 2,
+    "1/9": 0,
     "1/2": 0,
     "2/0": 3,
     "0/3": 4,
@@ -21,6 +22,7 @@ var path = map[string]int {
     "3/7": 8,
     "7/8": 3,
     "8/3": 9,
+    "3/9": 0,
 }
 
 const (
@@ -47,7 +49,10 @@ func main() {
 
 func getGraph(mp map[string]int) string {
     graph := "digraph G{\n"
+    graph += "0 [shape=box];\n"
+    graph += "1 [shape=box];\n"
 
+    set := make(map[string]struct{})
     var src, rec string
     var dst int
     for srcRec, d := range path {
@@ -55,8 +60,35 @@ func getGraph(mp map[string]int) string {
         splited := strings.Split(srcRec, "/")
         src, rec = splited[0], splited[1]
         graph += src + " -> " + rec + " [label=\"\"];\n"
+        break
+    }
+    set[src + "/" + rec] = struct{}{}
+    for {
+        src, rec = rec, strconv.Itoa(dst)
+        var ok bool
+        dst, ok = path[src + "/" + rec]
+        if !ok {
+            break
+        }
+        if _, ok := set[src + "/" + rec]; !ok {
+            set[src + "/" + rec] = struct{}{}
+            graph += src + " -> " + rec + " [label=\"\"];\n"
+        } else {
+            break
+        }
+    }
+    for srcRec, d := range path {
+        if _, ok := set[srcRec]; ok {
+            continue
+        }
+        dst = d
+        splited := strings.Split(srcRec, "/")
+        src, rec = splited[0], splited[1]
+        graph += src + " -> " + rec + " [label=\"\"];\n"
     }
     fmt.Println(dst, string(dst))
-    graph += rec + " -> " + strconv.Itoa(dst) + " [label=\"\"];\n}"
+    fmt.Println(graph)
+    //graph += rec + " -> " + strconv.Itoa(dst) + " [label=\"\"];\n}"
+    graph += "}"
     return graph
 }
